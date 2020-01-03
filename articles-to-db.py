3d26e92f48191baa46703610f20fffd46b6c15a7
Ben Schoenfeld <ben.schoenfeld@portsolutionintegrators.com>
2020-01-02 20:32:31 -0500

    Add articles to database script

---
+++ articles-to-db.py
@@ -0,0 +1,75 @@
+# TLA 1.a.i
+# Extract text from DOM Paths and write to database
+
+import os
+import re
+import xml.etree.ElementTree as ET
+from pprint import pprint
+from shutil import copyfile
+from utils.db import Database
+
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
+def get_text(tag_name):
+    tag = xml.find('.//*x:' + tag_name, ns)
+    return cleanhtml("".join(tag.itertext())) if tag is not None else None
+
+def get_p_text(tag_name):
+    tag = xml.find('.//*x:' + tag_name, ns)
+    p_tags = tag.findall('./x:p', ns)
+    body = ""
+    for p_tag in p_tags:
+        body += "".join(p_tag.itertext()) + "\n"
+    return cleanhtml(body)
+
+def get_sections():
+    pubdata = xml.findall('.//*x:pubdata', ns)
+    sections = []
+    for pd in pubdata:
+        if 'type' in pd.attrib and pd.attrib['type'] == 'web':
+            sections.append(pd.attrib['position.section'])
+    return sections
+
+def get_keywords():
+    keywords = xml.findall('.//*x:keyword', ns)
+    return [keyword.attrib['key'].encode("ascii", errors="ignore").decode() for keyword in keywords]
+
+def get_tag_attr(tag_name, attr_name):
+    tag = xml.find('.//*x:' + tag_name, ns)
+    if tag is None:
+        return tag
+    return tag.get(attr_name)
+
+def get_data_from_article():
+    return {
+        'docId': get_tag_attr('doc-id', 'id-string'),
+        'releaseDate': get_tag_attr('date.release', 'norm'),
+        'classifier': get_tag_attr('classifier', 'value'),
+        'location': get_tag_attr('location', 'location-code'),
+        'headline': get_text('hedline'),
+        'byline': get_text('byline'),
+        'publication': "",
+        'author': "",
+        'abstract': get_text('abstract'),
+        'body': get_p_text('body.content'),
+        'tagline': "",
+        'sections': get_sections(),
+        'keywords': get_keywords()
+    }
+
+
+ns = {'x': 'http://iptc.org/std/NITF/2006-10-18/'}
+db = Database()
+
+for root, dirs, files in os.walk('.\\articles'):
+    for f in files:
+        path = os.path.join(root, f)
+        xml = ET.parse(path).getroot()
+
+        article = get_data_from_article()
+        pprint(article)
+
+        db.add_article(article)
---
+++ utils/db.py
@@ -0,0 +1,108 @@
+import os
+from datetime import datetime, date
+from sqlalchemy import (create_engine, Boolean, Column,
+                        Date, DateTime, Integer, BigInteger,
+                        Float, String, Text, 
+                        Table, ForeignKey, Index)
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.pool import NullPool
+from pprint import pprint
+
+Base = declarative_base()
+
+articles_sections_assoc_table = Table('articles_sections', Base.metadata,
+    Column('articles_id', Integer, ForeignKey('articles.id')),
+    Column('sections_id', Integer, ForeignKey('sections.id'))
+)
+
+articles_keywords_assoc_table = Table('articles_keywords', Base.metadata,
+    Column('articles_id', Integer, ForeignKey('articles.id')),
+    Column('keywords_id', Integer, ForeignKey('keywords.id'))
+)
+
+class Article(Base):
+    __tablename__ = 'articles'
+    id = Column(Integer, primary_key=True)
+    filepath = Column(String)
+    docId = Column(String)
+    releaseDate = Column(DateTime)
+    classifier = Column(String)
+    location = Column(String)
+    headline = Column(Text)
+    byline = Column(String)
+    publication = Column(String)
+    author = Column(String)
+    abstract = Column(Text)
+    body = Column(Text)
+    tagline = Column(String)
+    sections = relationship(
+        "Section",
+        secondary=articles_sections_assoc_table,
+        back_populates="articles")
+    keywords = relationship(
+        "Keyword",
+        secondary=articles_keywords_assoc_table,
+        back_populates="articles")
+
+class Section(Base):
+    __tablename__ = 'sections'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    articles = relationship(
+        "Article",
+        secondary=articles_sections_assoc_table,
+        back_populates="sections")
+
+class Keyword(Base):
+    __tablename__ = 'keywords'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    articles = relationship(
+        "Article",
+        secondary=articles_keywords_assoc_table,
+        back_populates="keywords")
+
+#
+# Database class
+#
+class Database():
+    def __init__(self):
+        db_conn_str = 'postgresql://' + os.environ['PG_CONN_STR']
+        ssl_args = { 
+            'sslmode': 'verify-ca',
+            'sslrootcert': os.environ['PG_SERVER_CA'],
+            'sslcert': os.environ['PG_CLIENT_CERT'],
+            'sslkey': os.environ['PG_CLIENT_KEY']
+        }
+        self.engine = create_engine(db_conn_str, connect_args=ssl_args, poolclass=NullPool)
+        self.session = sessionmaker(bind=self.engine)()
+        Base.metadata.create_all(self.engine, checkfirst=True)
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
+
+    def disconnect(self):
+        self.session.close()
+    
+    def add_article(self, article):
+        sections = article['sections']
+        keywords = article['keywords']
+
+        del article['sections']
+        del article['keywords']
+
+        article_entity = Article(**article)
+
+        existing_sections = {s.name: s for s in self.session.query(Section).filter(Section.name.in_(sections)).all()}
+        existing_keywords = {k.name: k for k in self.session.query(Keyword).filter(Keyword.name.in_(keywords)).all()}
+
+        article_entity.sections = [existing_sections[s] if s in existing_sections else Section(name=s) for s in sections]
+        article_entity.keywords = [existing_keywords[k] if k in existing_keywords else Keyword(name=k) for k in keywords]
+
+        self.session.add(article_entity)
+        self.session.commit()
