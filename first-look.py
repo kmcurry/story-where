fa59dc7e6320e78239aefcda5bb3f4564394d6c5
Kevin Curry <kmcurry@gmail.com>
2020-01-03 18:55:15 -0500

    add comments to describe what this does

--- first-look.py
+++ first-look.py
@@ -1,10 +1,15 @@
+# Count and classify keywords and location place names found in newspaper articles.
+# Put results into CSV files.
 import csv
 import xml.etree.ElementTree as ET
 import os
 from shutil import copyfile
 
+# Newspaper Terms of North America namespace
 ns = {'x': 'http://iptc.org/std/NITF/2006-10-18/'}
 
+# Sample database of place names
+# TODO: Move to separate .json files or put in DB
 area = [
     'hampton roads',
     'north carolina',
@@ -89,11 +94,12 @@ locale = [
     'norfolk academy'
 ]
 
+# Count these attributes
 stories = 0
 with_location = 0
 with_keylist = 0
 with_section = 0
-keywords_count = {}
+keywords_count = {} # {keyword, count}
 sections_count = {}
 sections_no_keywords_count = {}
 
@@ -101,12 +107,13 @@ stories_with_wide_location = 0
 stories_with_gen_location = 0
 stories_with_spec_location = 0
 
+# for each Article, extract and count locations, keylists, and keywords
 for root, dirs, files in os.walk('.\\articles'):
     for f in files:
         path = os.path.join(root, f)
         xml = ET.parse(path).getroot()
         location = xml.find('.//*x:location', ns)
-        keylist = xml.find('.//*x:key-list', ns)
+        keylist = xml.find('.//*x:key-list', ns) # https://github.com/kmcurry/story-where/issues/2
         stories += 1
         if location is not None: with_location += 1
         if keylist is not None: with_keylist += 1
@@ -122,6 +129,8 @@ for root, dirs, files in os.walk('.\\articles'):
                 keywords_count[key] = 0
             keywords_count[key] += 1
 
+            # determine specificity of location by checking to see if the keyword 
+            # is found in one of our place name containers, above
             if key in area:
                 wide = True
             if key in cities:
@@ -142,6 +151,7 @@ for root, dirs, files in os.walk('.\\articles'):
             if 'type' in pd.attrib and pd.attrib['type'] == 'web':
                 has_section = True
                 section = pd.attrib['position.section']
+                # hardcoded news section?            vvv
                 if section == 'print_only/tabs_your_town_calendar': print(path)
                 if section not in sections_count:
                     sections_count[section] = 0
@@ -158,6 +168,7 @@ for root, dirs, files in os.walk('.\\articles'):
 
 print("DONE!")
 print(stories)
+# calculate percentages of all stories that are With X
 print("With location", (1.0* with_location) / stories * 100)
 print("With keylist", (1.0* with_keylist) / stories * 100)
 print("With section", (1.0* with_section) / stories * 100)
@@ -166,6 +177,7 @@ print("Wide", stories_with_wide_location)
 print("Gen", stories_with_gen_location)
 print("Specific", stories_with_spec_location)
 
+# save the results
 with open('keywords.csv', 'w') as f:
     for key in keywords_count.keys():
         f.write("%s,%s\n"%(key,keywords_count[key]))
