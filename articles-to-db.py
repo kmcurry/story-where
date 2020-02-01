1d88d024c1e6309b07a38e67ae21a41731cf7b82
Ben Schoenfeld <ben.schoenfeld@portsolutionintegrators.com>
2020-02-01 10:10:32 -0500

    Change nbsp to spaces

--- articles-to-db.py
+++ articles-to-db.py
@@ -20,7 +20,7 @@ from utils.db import Database
 # Returns a string
 def cleanhtml(raw_html):
   cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
-  cleantext = re.sub(cleanr, '', raw_html.encode("ascii", errors="ignore").decode().replace("\n", "|"))
+  cleantext = re.sub(cleanr, '', raw_html.replace(u'\xa0', ' ').encode("ascii", errors="ignore").decode().replace("\n", "|"))
   return cleantext
 
 # Get all text from named tag
