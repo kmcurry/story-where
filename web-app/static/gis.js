cac08d9a1dd1b1cc2cd3b31ac17a986c30e3278f
Kevin Curry <kmcurry@gmail.com>
2020-02-02 16:22:42 -0500

    add this lookup object

---
+++ web-app/static/gis.js
@@ -0,0 +1,30 @@
+var gis = {
+    Chesapeake: {
+
+    },
+    Hampton: {
+
+    },
+    NewportNews: {
+
+    },
+    Norfolk: {
+        boundary: "https://orfmaps.norfolk.gov/orfgis/rest/services/OpenData/Municipal/MapServer/2/",
+        neighborhoods: "https://raw.githubusercontent.com/blackmad/neighborhoods/master/norfolk.geojson", // THIRD PARTY
+        postal_codes: "https://opendata.arcgis.com/datasets/7c7145927f404d2d94cf86b62c6d6915_1.geojson"
+    },
+    Portsmouth: {
+
+    },
+    Suffolk: {
+
+    },
+    VirginiaBeach: {
+        boundary: "https://gismaps.vbgov.com/arcgis/rest/services/Basemaps/Administrative_Boundaries/MapServer/0/",
+        council: "https://gismaps.vbgov.com/arcgis/rest/services/Basemaps/VotingDistricts/MapServer/2/",
+        neighborhoods: "https://gis.data.vbgov.com/datasets/759ad66064974eab9a556d3a90efa1a1_23.geojson",
+        shelters: "https://gismaps.vbgov.com/arcgis/rest/services/Public_Safety/Emergency_Preparedness/MapServer/0/",
+        subdivisions: "https://gismaps.vbgov.com/arcgis/rest/services/Basemaps/PropertyInformation_VBgov/MapServer/23/",
+        postal_codes: "https://opendata.arcgis.com/datasets/82ada480c5344220b2788154955ce5f0_1.geojson"
+    }
+}
